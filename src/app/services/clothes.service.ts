97e3bf151380f3dcbcbd9f5eb2fda371d0be1601
mdorjbat <79872462+mdorjbat@users.noreply.github.com>
2021-05-20 01:13:08 -0500

    added grid to main page

--- src/app/services/clothes.service.ts
+++ src/app/services/clothes.service.ts
@@ -1,23 +1,47 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {UserService} from './user.service';
+
+const herokuUrl = 'https://secret-savannah-30262.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClothesService {
 
-  constructor() { }
+  constructor(private user: UserService, private http: HttpClient) { }
 
-  createClothes(): void{
-    const newProfile = {
-      firstName : this.firstName,
-      lastName : this.lastName,
-      age : this.age,
-      gender : this.gender,
-      size : this.size,
-      path : this.path
+  getClothes(): any{
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
     };
-    this.profileService.createProfile(newProfile).subscribe(responce => {
-      console.log(this.profile);
-    });
+    return this.http
+      .get(`${herokuUrl}/api/clothes`, requestOptions);
+  }
+
+  updateClothes(newClothes): any {
+    console.log(newClothes);
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+    };
+    return this.http
+      .put(`${herokuUrl}/api/clothes`, newClothes, requestOptions);
+  }
+  createClothes(newClothes): any {
+    console.log(newClothes);
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+    };
+    return this.http
+      .post(`${herokuUrl}/api/clothes`, newClothes, requestOptions);
   }
 }
